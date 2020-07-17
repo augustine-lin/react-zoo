2fe2aab825f37ac944c7b602f21a27d24dddca7d
Austin Lin <augustine.lin@mic.com.tw>
2020-07-17 17:40:44 +0800

    add EmptyTip component

---
+++ src/components/EmptyTip.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const EmptyTip = (props) => {
+  const { text = '目前有點空' } = props
+  return (
+    <div className="emptyTip">
+      <h3>{ text }</h3>
+      {
+        text === '目前有點空'
+        && (
+        <span className="ml-2">
+          <i className="far fa-flushed" />
+        </span>
+        )
+      }
+    </div>
+  )
+}
+
+export default EmptyTip
